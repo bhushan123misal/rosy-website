049ebe5d700942e5d05971520b903a788585a1d0
bhushan123misal <bhushan123misal@gmail.com>
2025-08-19 18:06:53 +0530

    Remove redeclaration of definedConfig in vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwind from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
-import { defineConfig } from 'vite'
 
 
 
