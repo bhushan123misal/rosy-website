938065cfe2bc834ab6e38282147184f477b4816b
bhushan123misal <bhushan123misal@gmail.com>
2025-08-19 18:01:21 +0530

    add build repo name to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwind from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rosy-project/',
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
