8b80029f668865a89d02609d30bd46b2832acc85
bhushan123misal <bhushan123misal@gmail.com>
2025-08-19 23:59:27 +0530

    Add deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwind from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
 
-
-
 export default defineConfig({
   plugins: [react()],
   base: '/rosy-project/',
